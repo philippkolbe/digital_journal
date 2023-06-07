7d38999283a7b05e1dcfa1121fb92c8e6b6240ff
PEK02 <philipp.kolbe@korasoft.net>
2023-06-07 23:15:13 +0200

    feat: firestore auth setup

--- app/lib/firebase_setup.dart
+++ app/lib/firebase_setup.dart
@@ -1,5 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> initializeFirebase() async {
   await Firebase.initializeApp();
+}
+
+Future<UserCredential> signInAnonymously() async {
+  final auth = FirebaseAuth.instance;
+  final userCredential = await auth.signInAnonymously();
+  return userCredential;
 }
---
+++ app/lib/providers/general_providers.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod/riverpod.dart';
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
+});
