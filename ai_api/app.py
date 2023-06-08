38026897fc75c86f95f3a4caef6e6afae4d0f2bc
PEK02 <philipp.kolbe@korasoft.net>
2023-06-08 14:49:28 +0200

    fix: app port + refactoring

--- ai_api/app.py
+++ ai_api/app.py
@@ -9,16 +9,16 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    user_message = request.json['message']
-    # Send user message to OpenAI and get response
-    response = openai.Completion.create(
-        engine='davinci',
-        prompt=user_message,
-        max_tokens=100
-    )
-    bot_response = response.choices[0].text.strip()
+  user_message = request.json['message']
+  # Send user message to OpenAI and get response
+  response = openai.Completion.create(
+    engine='davinci',
+    prompt=user_message,
+    max_tokens=100
+  )
+  bot_response = response.choices[0].text.strip()
 
-    return jsonify({'response': bot_response})
+  return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
-    app.run(port=6000)
+  app.run()
