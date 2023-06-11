3b691a8ba5929b9a2b1175c567bac9d7b05dc05d
PEK02 <philipp.kolbe@korasoft.net>
2023-06-11 11:46:25 +0200

    test: test data

---
+++ app/test/repositories/firebase_test_data.dart
@@ -0,0 +1,53 @@
+import 'package:app/models/chat_message.dart';
+import 'package:app/models/journal_entry.dart';
+import 'package:app/models/user.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
+const testUserId = 'id_test_user';
+const testUser = UserObj(id: testUserId, name: 'John Doe');
+
+const testSimpleJournalEntryId = 'id_simple_test_journal_entry';
+final testSimpleJournalEntry = JournalEntryObj.simple(
+  id: testSimpleJournalEntryId,
+  name: 'Simple Journal',
+  date: DateTime(2023, 1, 1, 10),
+) as SimpleJournalEntryObj;
+
+const testChatJournalEntryId = 'id_test_chat_journal_entry';
+final testChatJournalEntry = JournalEntryObj.chat(
+  id: testChatJournalEntryId,
+  name: 'Chat Journal',
+  date: DateTime(2023, 1, 1, 11),
+  goal: 'Test me'
+) as ChatJournalEntryObj;
+
+const testChatMessageId = 'id_test_message';
+final testChatMessageObj = ChatMessageObj(
+  isFromBot: false,
+  id: testChatMessageId,
+  date: DateTime(2023, 1, 1, 11, 5),
+  content: 'Hello, world!',
+);
+
+FakeFirebaseFirestore setupFakeFirestore({ bool user = false, bool journal = true, bool chat = true }) {
+  final firestore = FakeFirebaseFirestore();
+
+  if (user) {
+    final userDoc = firestore.collection('users').doc(testUserId);
+
+    userDoc.set(testUser.toDocument());
+
+    if (journal) {
+      final journalCollection = userDoc.collection('journalEntries');
+      final chatDoc = journalCollection.doc(testChatJournalEntryId);
+      chatDoc.set(testChatJournalEntry.toDocument());
+      journalCollection.doc(testSimpleJournalEntryId).set(testSimpleJournalEntry.toDocument());
+    
+      if (chat) {
+        chatDoc.collection('chatHistory').doc(testChatMessageId).set(testChatMessageObj.toDocument());
+      }
+    }
+  }
+
+  return firestore;
+}
