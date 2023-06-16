63242068ac5923493345cfdd4fe4252947d553f2
PEK02 <philipp.kolbe@korasoft.net>
2023-06-16 08:41:24 +0200

    feat: enabled chat from and to TextMessage to have interface to chat ui widget

--- app/lib/models/chat_message.dart
+++ app/lib/models/chat_message.dart
@@ -1,5 +1,7 @@
 import 'package:app/models/converters/date_converter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_chat_types/flutter_chat_types.dart' as chat_types;
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'chat_message.freezed.dart';
@@ -22,8 +24,28 @@ abstract class ChatMessageObj with _$ChatMessageObj {
     return ChatMessageObj.fromJson(doc.data() as Map<String, dynamic>);
   }
 
+  factory ChatMessageObj.fromTextMessage(chat_types.TextMessage message, { isFromBot = false }) {
+    return ChatMessageObj(
+      id: message.id,
+      content: message.text,
+      date: DateTime.fromMillisecondsSinceEpoch(message.createdAt!),
+      isFromBot: false,
+    );
+  }
+
   Map<String, dynamic> toDocument() {
     final json = toJson();
     return json..remove('id');
+  }
+
+  chat_types.TextMessage toTextMessage(chat_types.User user, chat_types.User botUser) {
+    assert(id != null, "Needs id to convert toTextMessage");
+
+    return chat_types.TextMessage(
+      id: id!,
+      text: content,
+      createdAt: date.millisecondsSinceEpoch,
+      author: isFromBot ? botUser : user,
+    );
   }
 }
