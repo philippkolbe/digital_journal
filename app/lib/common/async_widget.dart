8326eb0b56eb0587f088027aa4663981eb81a2c3
PEK02 <philipp.kolbe@korasoft.net>
2023-06-16 08:39:36 +0200

    feat: added async widget to wrap the AsyncValue building into a widget

---
+++ app/lib/common/async_widget.dart
@@ -0,0 +1,29 @@
+import 'package:app/common/error_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AsyncWidget<T> extends StatelessWidget {
+  final AsyncValue<T> asyncValue;
+  final Widget Function(T) buildWidget;
+  final void Function()? onRetryAfterError;
+  final String? retryText;
+
+  const AsyncWidget({
+    required this.asyncValue,
+    required this.buildWidget,
+    this.retryText,
+    this.onRetryAfterError,
+    super.key,
+  });
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: asyncValue.when(
+        data: (T data) => buildWidget(data),
+        error: buildErrorWidget(onRetry: onRetryAfterError, retryText: retryText),
+        loading: () => const CircularProgressIndicator(),
+      ),
+    );
+  }
+}
