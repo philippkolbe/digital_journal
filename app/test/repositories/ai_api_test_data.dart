7c477d71d1f3841ad6ad6a2abc986b7e8927e584
PEK02 <philipp.kolbe@korasoft.net>
2023-06-17 18:09:36 +0200

    test: added mock http client

---
+++ app/test/repositories/ai_api_test_data.dart
@@ -0,0 +1,6 @@
+const mockChatResponse = "Hi I'm a test.";
+const mockChatResponseBody = {
+  'successful': true,
+  'finish_reason': 'stop',
+  'response': mockChatResponse,
+};
---
+++ app/test/repositories/mock_http_client.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:app/providers/http_client_provider.dart';
+import 'package:app/repositories/ai_repository.dart';
+import 'package:http/src/response.dart';
+
+import 'ai_api_test_data.dart';
+
+class MockHttpClient implements AIHttpClient {
+  @override
+  final baseUrl = 'mock';
+
+  @override
+  Future<Response> get(String endpoint) {
+    throw UnimplementedError('Endpoint $endpoint unimplemented for GET from MockHttpClient');
+  }
+
+  @override
+  Future<Response> post(String endpoint, {Map<String, dynamic> body = const {}}) {
+    if (endpoint == AIRepository.chatRoute) {
+      return Future.value(Response(
+        jsonEncode(mockChatResponseBody),
+        200,
+      ));
+    } else {
+      throw UnimplementedError('Endpoint $endpoint unimplemented for POST to MockHttpClient');
+    }
+  }
+}
