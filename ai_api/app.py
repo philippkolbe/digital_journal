8a1828b79acb3c1c99537daba78d607969cf2399
PEK02 <philipp.kolbe@korasoft.net>
2023-06-17 18:00:53 +0200

    feat: implemented chat route

--- ai_api/app.py
+++ ai_api/app.py
@@ -2,23 +2,54 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import openai
 import os
+import logging
+
+
+def message_from_json(json):
+  return create_message(json['role'], json['content'])
+
+def create_message(role, content):
+  return {
+    'role': role,
+    'content': content,
+  }
 
 app = Flask(__name__)
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+system_message = create_message('system', 'You are a helpful journaling assistant.')
+
 @app.route('/chat', methods=['POST'])
 def chat():
-  user_message = request.json['message']
-  # Send user message to OpenAI and get response
-  response = openai.Completion.create(
-    engine='davinci',
-    prompt=user_message,
-    max_tokens=100
-  )
-  bot_response = response.choices[0].text.strip()
+  try:
+    user_id = request.json['user_id']
+    messages = map(message_from_json, request.json['messages'])
+    messages.insert(0, system_message)
 
-  return jsonify({'response': bot_response})
+    # Send user message to OpenAI and get response
+    response = openai.ChatCompletion.create(
+      engine='gpt-3.5-turbo',
+      messages=messages,
+      temperature=1.1,
+      user=user_id
+    )
+
+    choice = response.choices[0]
+    bot_response = choice.message.content.strip()
+    finish_reason = choice.finish_reason
+
+    return jsonify({
+      'success': True,
+      'response': bot_response,
+      'finish_reason': finish_reason,
+    })
+  except Exception as err:
+    logging.exception(e)
+    return jsonify({
+      'success': False,
+      'error': str(err),
+    })
 
 if __name__ == '__main__':
   app.run()
