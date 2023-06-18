948a8e91d859139a4ceb6c64a8331a5b521f83d9
PEK02 <philipp.kolbe@korasoft.net>
2023-06-18 09:15:34 +0200

    docs: chatbot controllers state vs. chatController

--- app/lib/controllers/chat_bot_controller.dart
+++ app/lib/controllers/chat_bot_controller.dart
@@ -22,6 +22,12 @@ final chatBotControllerProvider = StateNotifierProvider<ChatBotController, Async
   );
 });
 
+/// This Controller is responsible for fetching the bot response from the AIRepository and adding it to the Chat History.
+/// TODO: Think about if this should be a StateNotifier and what the state should be. Currently it is not being used.
+/// An idea is that the chat view shows both the chat history and the chat bots response. Then we can implement
+/// streaming the OpenAI response and showing it incrementally.
+/// On the other hand, the current solution allows the user to write chat messages while the bot is responding and still showing them in the right order.
+/// TODO: This should be handled correctly by waiting until one response has been finished before starting the next and including all the bots messages inbetween.
 class ChatBotController extends StateNotifier<AsyncValue<ChatMessageObj?>> {
   final String? _userId;
   final JournalEntryObj? _selectedJournalEntry;
