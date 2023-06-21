9454adabf4d2a261e01abbfa8020e0687134a002
PEK02 <philipp.kolbe@korasoft.net>
2023-06-21 22:11:07 +0200

    feat: added second list to tracking page

--- app/lib/views/tracking/tracking_page.dart
+++ app/lib/views/tracking/tracking_page.dart
@@ -1,5 +1,5 @@
-import 'package:app/common/challenge_progress_widget.dart';
 import 'package:app/models/progress.dart';
+import 'package:app/views/tracking/challenge_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,123 +11,73 @@ class TrackingPage extends ConsumerStatefulWidget {
 }
 
 class _TrackingPageState extends ConsumerState<TrackingPage> {
-  final _scrollListHeight = 130.0;
-  
-  final ScrollController _scrollController = ScrollController();
-  bool _showScrollIndicator = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_handleScroll);
-  }
-
-  @override
-  void dispose() {
-    _scrollController.removeListener(_handleScroll);
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _handleScroll() {
-    setState(() {
-      _showScrollIndicator = _scrollController.position.maxScrollExtent >
-          _scrollController.position.pixels;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final completedProgressObjs = [
+      ProgressObj(
+        title: 'Completed Challenge 1',
+        startDate: DateTime.now(),
+        durationInDays: 7,
+        daysCompleted: 7,
+      ),
+      ProgressObj(
+        title: 'Completed Challenge 2',
+        startDate: DateTime.now(),
+        durationInDays: 14,
+        daysCompleted: 14,
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Journey'),
         actions: [],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Open Challenges', style: TextStyle(fontSize: 20)),
-            const SizedBox(height: 5),
-            _buildChallengeList(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildChallengeList() {
-    final progressObjs = [
-      ProgressObj(
-        title: 'Journaling',
-        startDate: DateTime.now(),
-        durationInDays: 7,
-        daysCompleted: 3,
-      ),
-      ProgressObj(
-        title: 'Jogging',
-        startDate: DateTime.now(),
-        durationInDays: 21,
-        daysCompleted: 4,
-      ),
-      ProgressObj(
-        title: 'Jogging',
-        startDate: DateTime.now(),
-        durationInDays: 21,
-        daysCompleted: 4,
-      ),
-    ];
-
-    return SizedBox(
-      height: _scrollListHeight,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          _buildChallengeListView(progressObjs),
-          Visibility(
-            visible: _showScrollIndicator,
-            child: _buildScrollIndicator(),
+      body:  SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ChallengeList(
+                progressObjs: [
+                  ProgressObj(
+                    title: 'Journaling',
+                    startDate: DateTime.now(),
+                    durationInDays: 7,
+                    daysCompleted: 3,
+                  ),
+                  ProgressObj(
+                    title: 'Jogging',
+                    startDate: DateTime.now(),
+                    durationInDays: 21,
+                    daysCompleted: 4,
+                  ),
+                  ProgressObj(
+                    title: 'Jogging',
+                    startDate: DateTime.now(),
+                    durationInDays: 21,
+                    daysCompleted: 4,
+                  ),
+                ],
+                title: 'Open Challenges',
+                isActive: true,
+                onPressed: (data) {},
+                onSelected: (data, pressed) {},
+                emptyWidgetBuilder: () => const Text("Start with some challenges!"),
+              ),
+              const SizedBox(height: 15),
+              if (completedProgressObjs.isNotEmpty)
+                ChallengeList(
+                  progressObjs: completedProgressObjs,
+                  title: 'Completed Challenges',
+                  isActive: false,
+                  onPressed: (data) {},
+                  onSelected: (data, pressed) {},
+                ),
+            ],
           ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildChallengeListView(List<ProgressObj> progressObjs) {
-    return ListView(
-      controller: _scrollController,
-      scrollDirection: Axis.horizontal,
-      children: progressObjs
-          .map(_buildChallengeListEntry)
-          .toList(),
-    );
-  }
-
-  Widget _buildChallengeListEntry(progressObj) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: UserChallengeProgressCard(
-          progressObj: progressObj,
-          onPressed: (data) {},
         ),
-      ),
-    );
-  }
-
-  Widget _buildScrollIndicator() {
-    return Positioned(
-      right: 0.0,
-      top: _scrollListHeight/2 - 16,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey.withOpacity(0.7), // Adjust the color and opacity as desired
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        padding: const EdgeInsets.all(8.0),
-        child: const Icon(Icons.arrow_forward_ios, color: Colors.white),
       ),
     );
   }
