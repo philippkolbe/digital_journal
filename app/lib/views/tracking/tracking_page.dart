c5631a1ca48d1f5d1672f2614b7b4cbdd35d1763
PEK02 <philipp.kolbe@korasoft.net>
2023-06-22 16:44:55 +0200

    feat: show and add progressObjs in trackingPage

--- app/lib/views/tracking/tracking_page.dart
+++ app/lib/views/tracking/tracking_page.dart
@@ -1,3 +1,6 @@
+import 'package:app/common/async_widget.dart';
+import 'package:app/common/utils.dart';
+import 'package:app/controllers/progress_controller.dart';
 import 'package:app/models/progress.dart';
 import 'package:app/views/tracking/challenge_list.dart';
 import 'package:flutter/material.dart';
@@ -13,72 +16,70 @@ class TrackingPage extends ConsumerStatefulWidget {
 class _TrackingPageState extends ConsumerState<TrackingPage> {
   @override
   Widget build(BuildContext context) {
-    final completedProgressObjs = [
-      ProgressObj(
-        title: 'Completed Challenge 1',
-        startDate: DateTime.now(),
-        durationInDays: 7,
-        daysCompleted: 7,
-      ),
-      ProgressObj(
-        title: 'Completed Challenge 2',
-        startDate: DateTime.now(),
-        durationInDays: 14,
-        daysCompleted: 14,
-      ),
-    ];
+    final asyncProgressObjs = ref.watch(progressControllerProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Journey'),
-        actions: [],
+        actions: [
+          if (asyncProgressObjs.value != null) _buildCreateButton(),
+        ],
       ),
-      body:  SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: AsyncWidget(
+        asyncValue: asyncProgressObjs,
+        buildWidget: (progressObjs) {
+          return Column(
             children: [
-              ChallengeList(
-                progressObjs: [
-                  ProgressObj(
-                    title: 'Journaling',
-                    startDate: DateTime.now(),
-                    durationInDays: 7,
-                    daysCompleted: 3,
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ChallengeList(
+                        progressObjs: progressObjs,
+                        title: 'Open Challenges',
+                        isActive: true,
+                        onPressed: (data) {},
+                        onSelected: (data, pressed) {},
+                        emptyWidgetBuilder: () => const Text("Start with some challenges!"),
+                      ),
+                      const SizedBox(height: 15),
+                      if (progressObjs.isNotEmpty)
+                        ChallengeList(
+                          progressObjs: progressObjs,
+                          title: 'Completed Challenges',
+                          isActive: false,
+                          onPressed: (data) {},
+                          onSelected: (data, pressed) {},
+                        ),
+                    ],
                   ),
-                  ProgressObj(
-                    title: 'Jogging',
-                    startDate: DateTime.now(),
-                    durationInDays: 21,
-                    daysCompleted: 4,
-                  ),
-                  ProgressObj(
-                    title: 'Jogging',
-                    startDate: DateTime.now(),
-                    durationInDays: 21,
-                    daysCompleted: 4,
-                  ),
-                ],
-                title: 'Open Challenges',
-                isActive: true,
-                onPressed: (data) {},
-                onSelected: (data, pressed) {},
-                emptyWidgetBuilder: () => const Text("Start with some challenges!"),
-              ),
-              const SizedBox(height: 15),
-              if (completedProgressObjs.isNotEmpty)
-                ChallengeList(
-                  progressObjs: completedProgressObjs,
-                  title: 'Completed Challenges',
-                  isActive: false,
-                  onPressed: (data) {},
-                  onSelected: (data, pressed) {},
                 ),
+              ),
             ],
-          ),
-        ),
+          );
+        },
       ),
     );
+  }
+
+  Widget _buildCreateButton() {
+    return IconButton(
+      onPressed: _onAddChallenge,
+      icon: const Icon(Icons.add_circle_outlined),
+    );
+  }
+
+  Future<void> _onAddChallenge() async {
+    final progressController = ref.read(progressControllerProvider.notifier);
+    final challengeName = 'Journaling';
+    final durationInDays = 7;
+
+    await progressController.createProgress(ProgressObj(
+      title: challengeName,
+      startDate: floorDateToDay(DateTime.now()),
+      durationInDays: durationInDays,
+    ));
   }
 }
