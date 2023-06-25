4f55b2f3ead3262dfb0a70b50fbafa3af2c995c3
PEK02 <philipp.kolbe@korasoft.net>
2023-06-25 11:07:42 +0200

    feat: added provider that holds current day

---
+++ app/lib/providers/date_provider.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final currentDayProvider = StreamProvider<DateTime>((ref) {
+  final controller = StreamController<DateTime>();
+  
+  // Emit current day
+  _addNewDay(controller);
+
+  // Calculate the time until the next midnight
+  final now = DateTime.now();
+  final nextMidnight = DateTime(now.year, now.month, now.day + 1);
+  final timeUntilMidnight = nextMidnight.difference(now);
+
+  Timer(timeUntilMidnight, () {
+    // Update the current day at midnight
+    _addNewDay(controller);
+
+    // Set up the recurring timer for the next day
+    Timer.periodic(const Duration(days: 1), (_) => _addNewDay(controller));
+  });
+
+  ref.onDispose(() => controller.close());
+  return controller.stream;
+});
+
+void _addNewDay(StreamController controller) {
+  final now = DateTime.now();
+  final currentDay = DateTime(now.year, now.month, now.day);
+  controller.add(currentDay);
+}
