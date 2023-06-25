ccf9ea3f26305a70dbbe9bedd7dcda20df0abaab
PEK02 <philipp.kolbe@korasoft.net>
2023-06-25 11:06:49 +0200

    feat: added combineAsync and AsyncWidget2 for combining two AsyncValues in a widget

--- app/lib/common/async_widget.dart
+++ app/lib/common/async_widget.dart
@@ -1,11 +1,14 @@
-import 'package:app/common/error_widget.dart';
+import 'package:app/common/error_widget.dart' as err;
 import 'package:app/common/loading_widget.dart';
+import 'package:app/common/utils.dart';
+import 'package:tuple/tuple.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AsyncWidget<T> extends StatelessWidget {
   final AsyncValue<T> asyncValue;
   final Widget Function(T) buildWidget;
+  final Widget? Function(Object, StackTrace)? buildErrorWidget;
   final void Function()? onRetryAfterError;
   final String? retryText;
 
@@ -14,15 +17,46 @@ class AsyncWidget<T> extends StatelessWidget {
     required this.buildWidget,
     this.retryText,
     this.onRetryAfterError,
+    this.buildErrorWidget,
     super.key,
   });
- 
+  
   @override
   Widget build(BuildContext context) {
     return Center(
       child: asyncValue.when(
         data: (T data) => buildWidget(data),
-        error: buildErrorWidget(onRetry: onRetryAfterError, retryText: retryText),
+        error: buildErrorWidget ??
+          err.buildErrorWidget(onRetry: onRetryAfterError, retryText: retryText),
+        loading: () => const LoadingWidget(),
+      ),
+    );
+  }
+}
+
+class AsyncWidget2<T1, T2> extends StatelessWidget {
+  final AsyncValue<T1> asyncValue1;
+  final AsyncValue<T2> asyncValue2;
+  final AsyncValue<Tuple2<T1, T2>> asyncValue;
+  final Widget Function(T1, T2) buildWidget;
+  final void Function()? onRetryAfterError;
+  final String? retryText;
+
+  AsyncWidget2({
+    required this.asyncValue1,
+    required this.asyncValue2,
+    required this.buildWidget,
+    this.retryText,
+    this.onRetryAfterError,
+    super.key,
+  }) : asyncValue = combineAsync2(asyncValue1, asyncValue2);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: asyncValue.when(
+        data: (Tuple2<T1, T2> data) => buildWidget(data.item1, data.item2),
+        error: err.buildErrorWidget(onRetry: onRetryAfterError, retryText: retryText),
         loading: () => const LoadingWidget(),
       ),
     );
--- app/lib/common/utils.dart
+++ app/lib/common/utils.dart
@@ -1,3 +1,5 @@
+import 'package:tuple/tuple.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 
 var _uuid = const Uuid();
@@ -8,4 +10,23 @@ String generateUuid() {
 
 DateTime floorDateToDay(DateTime date) {
   return DateTime(date.year, date.month, date.day);
+}
+
+AsyncValue<Tuple2<T, R>> combineAsync2<T, R>(
+  AsyncValue<T> asyncOne,
+  AsyncValue<R> asyncTwo,
+) {
+  if (asyncOne is AsyncError) {
+    final error = asyncOne as AsyncError<T>;
+    return AsyncError(error.error, error.stackTrace);
+  } else if (asyncTwo is AsyncError) {
+    final error = asyncTwo as AsyncError<R>;
+    return AsyncError(error.error, error.stackTrace);
+  } else if (asyncOne is AsyncLoading || asyncTwo is AsyncLoading) {
+    return const AsyncLoading();
+  } else if (asyncOne is AsyncData && asyncTwo is AsyncData) {
+    return AsyncData(Tuple2<T, R>((asyncOne as AsyncData).value, (asyncTwo as AsyncData).value));
+  } else {
+    throw 'Unsupported case';
+  }
 }
