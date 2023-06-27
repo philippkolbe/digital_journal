c2729859400d7cdf3d38f0938965b07b63fddcd6
PEK02 <philipp.kolbe@korasoft.net>
2023-06-27 09:36:44 +0200

    feat: made isProgressActive public

--- app/lib/providers/active_progress_provider.dart
+++ app/lib/providers/active_progress_provider.dart
@@ -1,7 +1,7 @@
 import 'package:app/common/utils.dart';
 import 'package:app/controllers/progress_controller.dart';
 import 'package:app/models/progress.dart';
-import 'package:app/providers/date_provider.dart';
+import 'package:app/providers/current_day_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final activeProgressProvider = Provider((ref) {
@@ -14,14 +14,14 @@ final activeProgressProvider = Provider((ref) {
       final progression = data.item2;
       
       return progression.where(
-        (progressEntryObj) => _isProgressActive(progressEntryObj, currentDay),
+        (progressEntryObj) => isProgressActive(progressEntryObj, currentDay),
       ).toList();
     },
   );
 });
 
 
-bool _isProgressActive(ProgressObj progressObj, DateTime today) {
+bool isProgressActive(ProgressObj progressObj, DateTime today) {
   final startDate = progressObj.startDate;
   final durationInDays = progressObj.durationInDays;
   final endDate = startDate.add(Duration(days: durationInDays));
