0119b45ff6247e10ea71559dbaf0d1aabdc37834
PEK02 <philipp.kolbe@korasoft.net>
2023-06-28 00:47:10 +0200

    feat: encrypter for encrypting strings for db with local encryption key

---
+++ app/lib/providers/encrypter_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:app/providers/encryption_key_provider.dart';
+import 'package:encrypt/encrypt.dart' as encr;
+
+final encrypterProvider = Provider((ref) {
+  final encryptionKey = ref.watch(encryptionKeyProvider);
+  return Encrypter(encryptionKey);
+});
+
+class Encrypter {
+  final _iv = encr.IV.fromLength(16);
+  final encr.Key _encryptionKey;
+  late final encr.Encrypter _encrypter;
+
+  Encrypter(String key) :
+    _encryptionKey = encr.Key.fromUtf8(key) {
+      _encrypter = encr.Encrypter(encr.AES(_encryptionKey, mode: encr.AESMode.cbc));
+    }
+
+  String encrypt(String text) {
+    final encrypted = _encrypter.encrypt(text, iv: _iv);
+    return encrypted.base64;
+  }
+
+  String decrypt(String encryptedText) {
+    try {
+      final encrypted = encr.Encrypted.from64(encryptedText);
+      return _encrypter.decrypt(encrypted, iv: _iv);
+    } catch (e) {
+      // This means that it was not encoded originally. Should probably clean the db to prevent these cases.
+      return encryptedText;
+    }
+  }
+}
