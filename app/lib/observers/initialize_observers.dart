a7f9b2568d5097c2da400964c0aa40c59f640563
PEK02 <philipp.kolbe@korasoft.net>
2023-06-27 09:39:57 +0200

    feat: added a midnight observer to sync challenges

---
+++ app/lib/observers/initialize_observers.dart
@@ -0,0 +1,6 @@
+import 'package:app/observers/midnight_observer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void initializeObservers(WidgetRef ref) {
+  initializeMidnightObserver(ref);
+}
---
+++ app/lib/observers/midnight_observer.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:app/controllers/progress_controller.dart';
+import 'package:app/models/progress.dart';
+import 'package:app/providers/active_progress_provider.dart';
+import 'package:app/providers/current_day_provider.dart';
+import 'package:app/repositories/auth_repository.dart';
+import 'package:app/repositories/progress_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void initializeMidnightObserver(WidgetRef ref) {
+  final authStream = ref.read(authRepositoryProvider).authStateChangesStream;
+
+  StreamSubscription? sub;
+  authStream.listen((newUser) {
+    sub?.cancel();
+
+    if (newUser != null) {
+      sub = currentDayStream.listen((DateTime currentDay) async {
+        final progressRepository = ref.read(progressRepositoryProvider);      
+
+        List<ProgressObj> allProgressObjs = await progressRepository.readAllProgressions(newUser.uid);
+
+        final List<ProgressObj> newProgressObjs = [];
+        for (var progressObj in allProgressObjs) {
+          if (isProgressActive(progressObj, currentDay)) {
+            final daysSinceStart = currentDay.difference(progressObj.startDate).inDays;
+            final hasBeenCompleted = progressObj.hasBeenCompletedToday;
+            final daysCompleted = progressObj.daysCompleted;
+
+            final isNewDay = daysSinceStart > daysCompleted
+              || (daysCompleted == daysSinceStart && hasBeenCompleted);
+
+            if (isNewDay) {
+              newProgressObjs.add(progressObj.copyWith(
+                daysCompleted: daysSinceStart,
+                hasBeenCompletedToday: false,
+                streak: isNewDay && !hasBeenCompleted ? 0 : progressObj.streak,
+              ));
+            }
+          }
+        }
+
+
+        await Future.wait(newProgressObjs.map(
+          (updatedProgressObj) => progressRepository.updateProgress(
+            newUser.uid,
+            updatedProgressObj,
+          ),
+        ));
+
+        final controller = ref.read(progressControllerProvider.notifier);
+        await controller.loadProgressions();
+      });
+    }
+  });
+}
