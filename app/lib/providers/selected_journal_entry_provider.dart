547e3b68e7d3b31c52ec79ca54e761f142092d19
PEK02 <philipp.kolbe@korasoft.net>
2023-07-01 17:41:46 +0200

    fix: reset selected journal entry on user logout

--- app/lib/providers/selected_journal_entry_provider.dart
+++ app/lib/providers/selected_journal_entry_provider.dart
@@ -1,6 +1,11 @@
+import 'package:app/controllers/auth_controller.dart';
 import 'package:app/models/journal_entry.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final selectedJournalEntryProvider = StateProvider<AsyncValue<JournalEntryObj?>>(
-  (ref) => const AsyncData(null),
+  (ref) {
+    ref.watch(authControllerProvider);
+
+    return const AsyncData(null);
+  }
 );
