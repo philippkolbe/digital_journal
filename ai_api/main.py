4ecbf4e4206fb1e1c3bfafc8a8d31ad6b48288e1
PEK02 <philipp.kolbe@korasoft.net>
2023-07-02 02:38:19 +0200

    feat: enable frontend to pass system messges

--- ai_api/main.py
+++ ai_api/main.py
@@ -16,13 +16,14 @@ def create_message(role, content):
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-system_message = create_message('system', 'You are a friendly and motivating journaling companion within an app. The chat users now you already - you don’t have to introduce yourself. As users chat with you, you provide guidance for journaling and self-reflection. Your goal is to help users explore their emotions and personal development. By asking simple, non-intrusive questions, you empower users to delve deeper into their thoughts. Always ask one question at a time and await the users answer before asking more questions. Your first answer should be one simple question. Remember to maintain a quiet and supportive presence, akin to a trusted journal. Be very concise in answers.')
+default_system_message = create_message('system', 'You are a friendly and motivating journaling companion within an app. The chat users now you already - you don’t have to introduce yourself. As users chat with you, you provide guidance for journaling and self-reflection. Your goal is to help users explore their emotions and personal development. By asking simple, non-intrusive questions, you empower users to delve deeper into their thoughts. Always ask one question at a time and await the users answer before asking more questions. Your first answer should be one simple question. Remember to maintain a quiet and supportive presence, akin to a trusted journal. Be very concise in answers.')
 
 def chat(request: flask.Request) -> flask.Response:
   try:
     user_id = request.json['user_id']
     messages = [message_from_json(json) for json in request.json['messages']]
-    messages.insert(0, system_message)
+    if len(messages) == 0 or messages[0]['role'] != 'system':
+      messages.insert(0, default_system_message)
 
     # Send user message to OpenAI and get response
     response = openai.ChatCompletion.create(
