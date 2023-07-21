5d321d02b75f5975dcfa0b5c8c64e589188bc2c2
PEK02 <philipp.kolbe@korasoft.net>
2023-07-21 23:56:57 +0200

    feat: added initial form of notifications

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app/common/async_widget.dart';
 import 'package:app/observers/initialize_observers.dart';
 import 'package:app/views/navigation_view.dart';
@@ -6,11 +8,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_setup.dart';
 import 'package:app/controllers/auth_controller.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await initializeFirebase();
+
+  if (Platform.isIOS) {
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
   runApp(const ProviderScope(child: App()));
 }
 
@@ -24,9 +36,16 @@ class App extends ConsumerStatefulWidget {
 class _AppState extends ConsumerState<App> {
   @override
   void initState() {
+    super.initState();
+
     initializeObservers(ref);
     
-    super.initState();
+    FirebaseMessaging.onMessageOpenedApp.listen((message) =>
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: build),
+      )
+    );
   }
 
   @override
