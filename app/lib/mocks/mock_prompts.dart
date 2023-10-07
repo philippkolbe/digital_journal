b75ec312f3a71701b0ec6ecfc163e4fbe361c230
PEK02 <philipp.kolbe@korasoft.net>
2023-10-07 23:45:47 +0200

    test: added mock prompts

---
+++ app/lib/mocks/mock_prompts.dart
@@ -0,0 +1,6 @@
+import 'package:app/providers/prompts_providers.dart';
+
+final mockGeneralPrompts = {
+  GeneralPrompts.conversationSummary: 'Here is a summary: Nothing has happened so far.',
+  GeneralPrompts.summarizeChatMessage: 'Please summarize our conversation up to the message before this one.',
+};
