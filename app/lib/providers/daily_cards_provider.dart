5a615a63c1497a1ff74e1a82e2cdcfa1e4395fcf
PEK02 <philipp.kolbe@korasoft.net>
2023-11-01 21:44:29 +0100

    feat: added daily cards provider

---
+++ app/lib/providers/daily_cards_provider.dart
@@ -0,0 +1,19 @@
+import 'package:app/controllers/auth_controller.dart';
+import 'package:app/models/daily_card.dart';
+import 'package:app/providers/current_day_provider.dart';
+import 'package:app/repositories/daily_card_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dailyCardsProviderFamily = FutureProviderFamily<List<DailyCardObj>, DateTime>((ref, date) async {
+  final today = ref.watch(currentDayProvider).valueOrNull;
+  final userId = ref.watch(userIdProvider);
+  if (today == null || userId == null) {
+    return [];
+  }
+
+  if (date.isAfter(today)) {
+    return const [FutureDailyCardObj()];
+  }
+    
+  return await ref.read(dailyCardRepositoryProvider).readDailyCardsByDate(userId, date);
+});
